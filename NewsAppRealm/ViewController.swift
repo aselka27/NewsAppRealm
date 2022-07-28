d62339f0b9d9795dc77562542fb78d89a51c4ce5
aselka27 <asjumalieva@gmail.com>
2022-07-29 00:39:50 +0600

    Initial Commit

---
+++ NewsAppRealm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsAppRealm
+//
+//  Created by саргашкаева on 29.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
